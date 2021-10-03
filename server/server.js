3133d37b41248efc9a207cf14c76ee6146b227a2
somitgarhewal <somitgarhewal@gmail.com>
2021-10-03 15:35:24 +0530

    update url encoded limit to 50 mb

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ const port = 5000;
 
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
 app.use(bodyParser.json());
 
 app.use("/api", shopRoutes);
