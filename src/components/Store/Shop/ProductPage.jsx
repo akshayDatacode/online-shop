aaa559bab3f52c959441ed175b7303720a934f69
akshayDatacode <akshay.webllisto@gmail.com>
2021-10-26 18:11:04 +0530

    integrate get product api

--- src/components/Store/Shop/ProductPage.jsx
+++ src/components/Store/Shop/ProductPage.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+// import { useDispatch } from 'react-redux'
 
-const ProductPage = () => {
+import { getOrder } from '../../actions/apiServices'
+
+const ProductPage = ({
+  match: {
+    params: { id },
+  },
+}) => {
+  const [productDetails, setProductDetails] = useState({})
+
+  useEffect(() => {
+    getOrder(id).then((res) => {
+      if (res.success) {
+        setProductDetails(res.data)
+      }
+    })
+  }, [])
+
   return (
     <>
       <h1>ProductPage</h1>
+      {
+        productDetails ?
+          <h1 className="text-center">Loading...</h1>
+          :
+          <div className="row mx-0">
+            <div className="col-md-6 col-12 ">
+
+            </div>
+            <div className="col-md-6 col-12 ">
+
+            </div>
+          </div>
+      }
     </>
   )
 }
