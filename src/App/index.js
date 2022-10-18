979d90fa5640ef247a0de2be600a8cb3730c24dd
yashpandey061 <yashpandey061@gmail.com>
2022-10-18 16:09:21 +0530

    handle to set logined inuse untill token valid

--- src/App/index.js
+++ src/App/index.js
@@ -1,12 +1,31 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Header from "./Header";
 import CartDrawer from "./Header/CartDrawer";
 import BackDrop from "./Header/BackDrop";
 import AppRoutes from "./AppRoutes";
 import { ToastContainer } from "react-toastify";
+import jwtDecode from "jwt-decode";
+import { setLoginUser } from "../pages/User/actions";
+import { useDispatch } from "react-redux";
+import { toast } from 'react-toastify';
 
 const App = () => {
+  const dispatch = useDispatch()
   const [cartDrawerOpen, setCartDrawerOpen] = useState(false);
+
+  useEffect(() => {
+    let token = localStorage.getItem('token')
+    try {
+      let user = jwtDecode(token)
+      if (user?.email) {
+        dispatch(setLoginUser(token))
+      }
+    } catch (err) {
+      toast.error('Token Invalid', {
+        position: toast.POSITION.TOP_CENTER
+      })
+    }
+  }, [])
 
   const drawerToggleClickHandler = () => {
     setCartDrawerOpen((prevState) => {
