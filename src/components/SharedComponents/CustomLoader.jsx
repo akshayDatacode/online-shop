503687b211b07d0eac8addc09842fa3ae182f1c6
akshayDatacode <akshay.webllisto@gmail.com>
2025-10-04 02:13:03 +0530

    Refactor code with prpotype loader file

--- src/components/SharedComponents/CustomLoader.jsx
+++ src/components/SharedComponents/CustomLoader.jsx
@@ -1,23 +1,31 @@
-import React from 'react'
+import React from 'react';
+import PropTypes from 'prop-types';
 import Loader from "react-loader-spinner";
 
 const CustomLoader = ({
-  color,
-  type,
-}) => {
-  return (
-    <div className="row m-0 d-flex justify-content-center">
-      <div className="col-12 align-items-center text-center ">
-        <Loader
-          type={type || "ThreeDots"}
-          color={color || "#797676"}
-          height={50}
-          width={50}
-          timeout={1000} //1 secs
-        />
-      </div>
-    </div>
-  )
-}
+  color = "#797676",
+  type = "ThreeDots",
+  height = 50,
+  width = 50,
+  timeout = 1000
+}) => (
+  <div className="loader-container">
+    <Loader
+      type={type}
+      color={color}
+      height={height}
+      width={width}
+      timeout={timeout}
+    />
+  </div>
+);
 
-export default CustomLoader 
+CustomLoader.propTypes = {
+  color: PropTypes.string,
+  type: PropTypes.string,
+  height: PropTypes.number,
+  width: PropTypes.number,
+  timeout: PropTypes.number
+};
+
+export default CustomLoader;
