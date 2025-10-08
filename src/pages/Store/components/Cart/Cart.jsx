07f2ccfa67a15d84ed580a19b8eaf7f742b41632
akshayDatacode <akshay.webllisto@gmail.com>
2025-10-09 01:37:42 +0530

    Refacor cart page

--- src/pages/Store/components/Cart/Cart.jsx
+++ src/pages/Store/components/Cart/Cart.jsx
@@ -1,184 +1,132 @@
-import React, { useEffect, useState } from 'react'
-import { useSelector, useDispatch } from 'react-redux'
-import { Link } from 'react-router-dom'
-import { toast } from 'react-toastify'
-import BootstrapTable from 'react-bootstrap-table-next'
+import React, { useEffect, useState, useCallback } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { toast } from 'react-toastify';
+import PropTypes from 'prop-types';
 
-import { deleteCartItem, clearCart, setQuantity, addOrder, completeCartOrder } from '../../actions'
-import CompleteOrderModal from './CompleteOrderModal'
-import { getColumns, daysCodeList } from './helpers'
+import { deleteCartItem, clearCart, setQuantity } from '../../actions';
+import { addOrder, completeCartOrder } from '../../actions';
+import CompleteOrderModal from './CompleteOrderModal';
+import CartItem from './CartItem';
+import CartTotal from './CartTotal';
 
-const Cart = ({
-  backDropClickHandler
-}) => {
-  const dispatch = useDispatch()
-
-  const cartList = useSelector(({ shop }) => shop.cartList)
-  const [total, setTotal] = useState(0)
-  const [openModal, setOpenModal] = useState(false)
-  const [tempUserEmail, setTempUserEmail] = useState()
-  const [showCross, setShowCross] = useState(false)
+const Cart = ({ backDropClickHandler }) => {
+  const dispatch = useDispatch();
+  const cartList = useSelector(({ shop }) => shop.cartList);
+  
+  const [total, setTotal] = useState(0);
+  const [openModal, setOpenModal] = useState(false);
+  const [tempUserEmail, setTempUserEmail] = useState('');
+  const [showCross, setShowCross] = useState(false);
 
   useEffect(() => {
-    let t = 0
     if (cartList) {
-      cartList.map((item) => {
-        t = t + (item.price * item.quantity)
-      })
+      const newTotal = cartList.reduce((sum, item) => 
+        sum + (item.price * item.quantity), 0);
+      setTotal(newTotal);
     }
-    setTotal(t)
-  }, [cartList])
+  }, [cartList]);
 
-  const handleRemoveFromCart = (item) => {
-    dispatch(deleteCartItem(item))
+  const handleRemoveFromCart = useCallback((item) => {
+    dispatch(deleteCartItem(item));
     toast.error('Item removed from cart', {
       position: toast.POSITION.TOP_CENTER
-    })
-  }
+    });
+  }, [dispatch]);
 
-  const toggleModal = () => {
-    setOpenModal(!openModal)
-  }
+  const toggleModal = useCallback(() => {
+    setOpenModal(prev => !prev);
+  }, []);
 
-  const handleConfirmOrder = (email) => {
-    setTempUserEmail(email)
-    // setUserAction(action)
+  const handleConfirmOrder = useCallback(async (email) => {
+    setTempUserEmail(email);
+    const orderData = { email, cartList, total };
+    
+    try {
+      const [addResponse, completeResponse] = await Promise.all([
+        addOrder(orderData),
+        completeCartOrder(orderData)
+      ]);
 
-    const data = {
-      email,
-      cartList,
-      total,
+      if (addResponse && completeResponse) {
+        dispatch(clearCart());
+        toggleModal();
+      }
+    } catch (error) {
+      toast.error('Failed to complete order. Please try again.');
     }
-    addOrder(data).then((res) => {
-      if (res) {
-        dispatch(clearCart())
-        setOpenModal(!openModal)
-      }
-    })
-    completeCartOrder(data).then((res) => {
-      if (res) {
-        dispatch(clearCart())
-        setOpenModal(!openModal)
-      }
-    })
-  }
+  }, [cartList, total, dispatch, toggleModal]);
 
-  const handleSetQuantity = (qty, id) => {
-    return dispatch(setQuantity({ qty, id }))
-  }
+  const handleSetQuantity = useCallback((qty, id) => {
+    dispatch(setQuantity({ qty, id }));
+  }, [dispatch]);
 
-  let columns = getColumns(handleRemoveFromCart, handleSetQuantity)
+  const renderEmptyCart = () => (
+    <div className="d-flex align-items-center my-5">
+      {!tempUserEmail && (
+        <h6 className="text-center">
+          Cart Empty Please,
+          <span className="mx-2" onClick={backDropClickHandler}>
+            <Link to="/">Add Item into Cart</Link>
+          </span>
+        </h6>
+      )}
+    </div>
+  );
+
+  const renderOrderConfirmation = () => (
+    <div className="row mx-0">
+      <div className="col-12 text-center">
+        <h3>Thanks for Shopping with us!</h3>
+        <small>Your invoice will be sent to your email</small>
+        <p><Link to="/">Order Again</Link></p>
+      </div>
+    </div>
+  );
 
   return (
-    <>
-      <div className="row p-2 mx-0">
-        <div className="col-12 border p-0">
-          <div className="cart-list">
-            {cartList && cartList.length ?
-              // <div className="table table-responsive">
-              //   <BootstrapTable
-              //     keyField='id'
-              //     bordered={true}
-              //     data={cartList}
-              //     columns={columns}
-              //     search
-              //     hover={true}
-              //   />
-              // </div>
-              cartList.map((item, i) => (
-                <div className="row mx-0 my-4 pb-2 border-bottom" onMouseLeave={() => setShowCross(false)} onMouseEnter={() => setShowCross(i)} key={i}>
-                  <div className="col-4">
-                    <img
-                      src={item.image}
-                      className="rounded-circle"
-                      height="50"
-                      width="50"
-                      alt="loading..."
-                    />
-                  </div>
-                  <div className="col-8">
-                    <div className="d-flex justify-content-between">
-                      <h6>{item.title}</h6>
-                      {
-                        showCross === i &&
-                        <i
-                          onClick={() => handleRemoveFromCart(item)}
-                          className="fal fa-times text-danger"
-                        />
-                      }
-                    </div>
-                    <span className="text-success">₹{item.price}</span>
-                    <div className="counter-toggle">
-                      {item.quantity > 1 && (
-                        <span
-                          className="px-1 text-white bg-danger rounded cursor-pointer"
-                          onClick={() => handleSetQuantity(item.quantity - 1, item._id)}
-                        >
-                          -
-                        </span>
-                      )}
-                      <span className="px-2">{item.quantity}</span>
-                      <span
-                        className="px-1 text-white bg-primary rounded cursor-pointer"
-                        onClick={() => handleSetQuantity(item.quantity + 1, item._id)}
-                      >
-                        +
-                      </span>
-                    </div>
-                  </div>
-                </div>
-              ))
-              :
-              <div className="d-flex align-items-center my-5">
-                {
-                  !tempUserEmail &&
-                  <h6 className="text-center">Cart Empty Please,
-                    <span className="mx-2" onClick={() => backDropClickHandler()}><Link to="/">Add Item into Cart</Link></span>
-                  </h6>
-                }
-              </div>
-            }
-          </div>
-          {
-            cartList && cartList.length ?
-              <>
-                <div className="row mx-0 py-2 px-4 bg-dark align-items-center">
-                  <div className="col-8">
-                    <h6 className="mb-0 text-white">Sub Total</h6>
-                  </div>
-                  <div className="col-4 text-center">
-                    <h6 className="mb-0 text-success text-nowrap">{total} <i className="fal fa-rupee-sign" /></h6>
-                  </div>
-                </div>
-                <div className="row mx-0 p-md-3 mt-3" >
-                  <div className="col-12 text-center">
-                    <button onClick={() => toggleModal()} className="btn btn-success">Complete Order</button>
-                  </div>
-                </div>
-              </>
-              :
-              ""
-          }
-          {
-            tempUserEmail &&
-            <div className="row mx-0">
-              <div className="col-12 text-center">
-                <h3>Thanks for Shopping with us!</h3>
-                <small>Your invoice will send to your email</small>
-                <p><Link to="/">Order Again</Link></p>
-              </div>
-            </div>
-          }
+    <div className="row p-2 mx-0">
+      <div className="col-12 border p-0">
+        <div className="cart-list">
+          {cartList?.length ? (
+            cartList.map((item, i) => (
+              <CartItem
+                key={item._id}
+                item={item}
+                showCross={showCross === i}
+                onMouseLeave={() => setShowCross(false)}
+                onMouseEnter={() => setShowCross(i)}
+                onRemove={handleRemoveFromCart}
+                onUpdateQuantity={handleSetQuantity}
+              />
+            ))
+          ) : (
+            renderEmptyCart()
+          )}
         </div>
-        <CompleteOrderModal
-          open={openModal}
-          total={total}
-          toggle={toggleModal}
-          onSubmit={handleConfirmOrder}
-        />
-      </div>
-    </>
-  )
-}
 
-export default Cart
+        {cartList?.length > 0 && (
+          <CartTotal 
+            total={total} 
+            onComplete={toggleModal}
+          />
+        )}
+
+        {tempUserEmail && renderOrderConfirmation()}
+      </div>
+
+      <CompleteOrderModal
+        open={openModal}
+        total={total}
+        toggle={toggleModal}
+        onSubmit={handleConfirmOrder}
+      />
+    </div>
+  );
+};
+
+Cart.propTypes = {
+  backDropClickHandler: PropTypes.func.isRequired
+};
+
+export default Cart;
